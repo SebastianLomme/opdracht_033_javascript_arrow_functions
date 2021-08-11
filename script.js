be435d7e078f5fb823ed6899922df44ba38a1164
Sebastian Lomme <sebastian.lomme@gmail.com>
2021-08-11 13:22:59 +0200

    verscillende arrow functions gemaakt

--- script.js
+++ script.js
@@ -2,3 +2,19 @@
 const ikRockArrowFunctions =  () => console.log("Joe, ik rock de arrow functions!");
 
 ikRockArrowFunctions()
+
+const fivePlusSeven = () => 5 + 7
+
+console.log(fivePlusSeven())
+
+const myFunction = (a, b) => a + b;
+
+console.log(myFunction(1, 2))
+
+const addFive = a => a + 5;
+
+console.log(addFive(5))
+
+const createOjbject = () => ({greeting: "hoi"})
+
+console.log(createOjbject)
